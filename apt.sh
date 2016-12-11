32f942d79d316b68c1795794f292ff88be6abdfe
C Brightly <chris.brightly@gmail.com>
2016-12-11 16:18:31 -0500

    Upgrade to more comprehensive set up commands

--- apt.sh
+++ apt.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 #
-# apt.sh: apt-get automation script
-# 	eases system maintainance with apt-get using a simple BASH script
-# by C. Brightly [https://github.com/cbrightly]
-# available at github [https://github.com/cbrightly/apt-script]
-# see LICENSE for information on usage, distribution and other rights
+# By cbrightly @ github
+#
+# Description:
+#   apt.sh: automated self-cleaning package updates!
+#
+#   Update of available packages, upgrade of
+#   any obsolete installed packages + full
+#   dist-upgrade run by apt,  followed by
+#   apt's built-in self-cleaning cycles
 
+# Generate list of installed packages (for backup reference)
+sudo dpkg -l | grep '^ii' | awk -F" " '{printf "%-40s %s\n", $2, $3}' >/root/installed_packages.txt
+
+# Update repositories
 sudo apt-get update;
+#Perform Upgrade
 sudo apt-get upgrade;
+sudo apt-get dist-upgrade
+# Clean + purgeold/obsoleted packages
+sudo apt-get autoclean &>/dev/null;
 sudo apt-get autoremove;
-sudo apt-get autoclean;
+
